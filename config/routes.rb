a5dfdbcdd6cd6ec8df17715ad8dcc0d8b36597d7
Michele de Bruyn <debruyn.mm@gmail.com>
2016-04-17 16:55:39 +0200

    Create Pages controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'experience', to: 'pages#experience'
+  get 'contact', to: 'pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
